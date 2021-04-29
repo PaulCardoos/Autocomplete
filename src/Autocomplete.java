e88e6f7248723e7628a8ddb5da5853fd6fae1f5e
paulCardoos <paul.cardoos001@umb.edu>
2021-04-29 00:03:44 -0400

    added image for readme

--- src/Autocomplete.java
+++ src/Autocomplete.java
@@ -1,54 +1,60 @@
 import java.util.Arrays;
 import java.util.Comparator;
 
+// A data type that provides autocomplete functionality for a given set of 
+// string and weights, using Term and BinarySearchDeluxe. 
 public class Autocomplete {
-    private final Term[] terms; //array of terms
-    // Constructs an autocomplete data structure from an array of terms.
+    private final Term[] terms;   // The set of terms
+
+    // Initialize the data structure from the given array of terms.
     public Autocomplete(Term[] terms) {
-        //data validation
-        if(terms == null)
-            throw new NullPointerException("terms is null");
-        //defensive copy of terms
+        if (terms == null) throw new NullPointerException();
+        // Defensive copy of terms[]
         this.terms = Arrays.copyOf(terms, terms.length);
-        //lexicographical sort of this.terms
+        // Lexicographical sort of terms[]
         Arrays.sort(this.terms);
     }
 
-    // Returns all terms that start with prefix, in descending order of their weights.
+    // All terms that start with the given prefix, in descending order of
+    // weight.
     public Term[] allMatches(String prefix) {
-        //data validation
-        if(prefix == null)
-            throw new NullPointerException("prefix is null");
+        if (prefix == null) throw new NullPointerException();
 
         Term term = new Term(prefix);
+        // Comparator object byPrefixOrder of length prefix
+        Comparator<Term> pOrder = Term.byPrefixOrder(prefix.length());
+        int fi = BinarySearchDeluxe.firstIndexOf(this.terms, term, pOrder);
+        int li = BinarySearchDeluxe.lastIndexOf(this.terms, term, pOrder);
+        // Number of terms n is difference between firstIndex
+        // and lastIndex + 1
+        int n = li - fi + 1;
 
-        //comparator object for binary search
-        Comparator<Term> prefixOrder = Term.byPrefixOrder(prefix.length());
-
-        //index of the first term that starts with prefix
-        int firstIndex = BinarySearchDeluxe.firstIndexOf(this.terms, term, prefixOrder);
-        int lastIndex = BinarySearchDeluxe.lastIndexOf(this.terms, term, prefixOrder);
-
-        //number of keys that match the prefix
-        int N = lastIndex - firstIndex + 1;
-
-        //initialize array match of length N
-        Term[] matches = new Term[N];
-
-        //loop through all terms that start with the prefix and store in array named matches
-        System.arraycopy(this.terms, firstIndex, matches, 0, N);
-
-        //sort matches in reverse order of weight
+        // New array matches
+        Term[] matches = new Term[n];
+        for (int i = 0; i < n; i++) {
+            // Store terms into matches from start to end of matched indices
+            matches[i] = this.terms[fi + i];
+        }
+        // Sort matches by ReverseWeightOrder
         Arrays.sort(matches, Term.byReverseWeightOrder());
-
         return matches;
-
     }
 
-    // Unit tests the data type. [DO NOT EDIT]
+    // The number of terms that start with the given prefix.
+    public int numberOfMatches(String prefix) {
+        if (prefix == null) throw new NullPointerException();
+        // Number of terms n is difference between firstIndex
+        // and lastIndex + 1
+        Term term = new Term(prefix);
+        Comparator<Term> pOrder = Term.byPrefixOrder(prefix.length());
+        int fi = BinarySearchDeluxe.firstIndexOf(this.terms, term, pOrder);
+        int li = BinarySearchDeluxe.lastIndexOf(this.terms, term, pOrder);
+        return li - fi + 1;
+    }
+
+    // Entry point. [DO NOT EDIT]
     public static void main(String[] args) {
         String filename = args[0];
-        int k = Integer.parseInt(args[1]);
         In in = new In(filename);
         int N = in.readInt();
         Term[] terms = new Term[N];
@@ -58,24 +64,27 @@ public class Autocomplete {
             String query = in.readLine();
             terms[i] = new Term(query.trim(), weight);
         }
+        int k = Integer.parseInt(args[1]);
         Autocomplete autocomplete = new Autocomplete(terms);
-        StdOut.print("Enter a prefix (or ctrl-d to quit): ");
+        StdOut.print("Enter a prefix: ");
+        boolean firstLoop = true;
         while (StdIn.hasNextLine()) {
             String prefix = StdIn.readLine();
             Term[] results = autocomplete.allMatches(prefix);
-            String msg = " matches for \"" + prefix + "\", in descending order by weight:";
-            if (results.length == 0) {
-                msg = "No matches";
-            } else if (results.length > k) {
-                msg = "First " + k + msg;
-            } else {
-                msg = "All" + msg;
-            }
+            String msg = " matches, in descending order by weight:";
+            if (results.length == 0) msg = "No matches";
+            else if (results.length > k) msg = "First " + k + msg;
+            else msg = "All" + msg;
             StdOut.printf("%s\n", msg);
             for (int i = 0; i < Math.min(k, results.length); i++) {
-                StdOut.println("  " + results[i]);
+                StdOut.println(results[i]);
             }
-            StdOut.print("Enter a prefix (or ctrl-d to quit): ");
+            StdOut.print("\nEnter a prefix, or press <ctrl-d> to quit : ");
+            if (firstLoop) {
+                StdOut.print("\n(Unless you're using IntelliJ to run. Then the "
+                        + "stop button will quit)\nThat prefix, please: ");
+                firstLoop = false;
+            }
         }
     }
 }
